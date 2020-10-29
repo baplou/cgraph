e8b6ca4d10a45c87e91db9de10e39a0999b6e281
pablo <26plau26@gmail.com>
2020-10-29 17:08:23 -0500

    feat: Colored bar graphs

--- cgraph/bar.py
+++ cgraph/bar.py
@@ -1,12 +1,22 @@
 class BarGraph:
-  def __init__(self, lowest_num, highest_num, nums, labels, max_spaces):
+  def __init__(self, lowest_num, highest_num, nums, labels, max_spaces, color=True):
     self.lowest_num = lowest_num
     self.highest_num = highest_num
     self.nums = nums
     self.labels = labels
     self.max_spaces = max_spaces
+    self.color = color
 
     self.lines = []
+    self.colors = {
+      "aqua":"\x1b[7;36;46m",
+      "pink":"\x1b[7;35;45m",
+      "blue":"\x1b[7;34;44m",
+      "yellow":"\x1b[7;33;43m",
+      "green":"\x1b[7;32;42m",
+      "red":"\x1b[7;31;41m",
+      "reset":"\x1b[0m"
+    }
 
     self.setup()
 
@@ -25,31 +35,41 @@ class BarGraph:
         print("cgraph error: number has to be equal to or more than the lowest number argument")
         quit()
 
-    max_length = self.longest_label(self.labels)
+    label_max_length = self.longest_label(self.labels)
 
     # space and label (spaces are uneven)
     for label in self.labels:
-      self.lines.append(f" {label}")
+      self.lines.append(f"{label}")
 
     # the length of every item in self.lines is equal
-    counter = 0
-    for line in self.lines:
-      counter += 1
-      if len(line) - 1 < max_length:
-        for i in range(max_length - (len(line) - 1)):
-          line += " "
+    for i in range(len(self.lines)):
+      if len(self.lines[i]) < label_max_length:
+        for x in range(label_max_length - (len(self.lines[i]))):
+          self.lines[i] += " "
 
-      line += "  "
-      self.lines[counter - 1] = line
+      if self.color:
+        if self.odd(i):
+          self.lines[i] += f" {self.colors['aqua']}"
+        else:
+          self.lines[i] += f" {self.colors['red']}"
+      else:
+        self.lines[i] += " "
 
-    # spaces/characters depending on the given values
+    # how many characters to fill each line with
     character_per_num = int(self.max_spaces/(self.highest_num - self.lowest_num))
-
     for i in range(len(self.nums)):
       for x in range(self.nums[i]):
         for c in range(character_per_num):
           self.lines[i] += "x"
 
+    # adding the final number for each line
+    for i in range(len(self.lines)):
+      if self.color:
+        self.lines[i] += f"{self.colors['reset']} {float(self.nums[i])}"
+      else:
+        self.lines[i] += f" {float(self.nums[i])}"
+
+  def show(self):
     for line in self.lines:
       print(line)
 
@@ -61,5 +81,13 @@ class BarGraph:
 
     return max(slt)
 
+  @staticmethod
+  def odd(number):
+    if number % 2 == 0:
+      return False
+    else:
+      return True
+
 if __name__ == "__main__":
   b = BarGraph(1, 7, [7, 4, 5, 6], ["Math", "Science", "English", "PE"], 50)
+  b.show()
