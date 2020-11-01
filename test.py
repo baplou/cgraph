b7494f0314f0cac43c7a130f32155d4134f2d65b
pablo <26plau26@gmail.com>
2020-11-01 00:52:31 -0500

    feat: Added test files

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from cgraph.graph import CoordinateGraph
+from cgraph.bar import BarGraph
+
+# tests
+#import tests.graph.test_file_name
+#import tests.bar.test_file_name
+import tests.bar.errors
